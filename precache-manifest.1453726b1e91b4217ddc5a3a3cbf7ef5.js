f142071d920829cc46ad200a073c0055fef1cd7a
Tommy Ballo JobbPC <tommy.ballo1995@gmail.com>
2018-12-14 15:26:26 +0100

    Updates

---
+++ precache-manifest.1453726b1e91b4217ddc5a3a3cbf7ef5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b015688d8763c7796b9c",
+    "url": "/pc-digg/static/js/runtime~main.b015688d.js"
+  },
+  {
+    "revision": "d584b871dcd8c0ab898a",
+    "url": "/pc-digg/static/js/main.d584b871.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/pc-digg/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "d584b871dcd8c0ab898a",
+    "url": "/pc-digg/static/css/main.d35e8f58.chunk.css"
+  },
+  {
+    "revision": "098b3302219d271861f94db35293a826",
+    "url": "/pc-digg/index.html"
+  }
+];
