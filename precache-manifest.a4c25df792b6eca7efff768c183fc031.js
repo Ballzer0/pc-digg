0daa4f033cb7a8376cb47a5cfa34a0eb64a68f8c
Tommy Ballo JobbPC <tommy.ballo1995@gmail.com>
2018-12-18 14:15:46 +0100

    Updates

---
+++ precache-manifest.a4c25df792b6eca7efff768c183fc031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "495043903add56a34712",
+    "url": "/static/js/main.49504390.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "495043903add56a34712",
+    "url": "/static/css/main.c68fc9f5.chunk.css"
+  },
+  {
+    "revision": "7c2b07b1783562363340c58d7b1d21fe",
+    "url": "/index.html"
+  }
+];
